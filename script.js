b6142dc63a493ee88192de663c8b4877f6c3a061
Chaotic-Potato <andy825825@gmail.com>
2016-11-10 21:59:24 -0600

    Fixed git mistake

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ var Game = {
 
 var State = function (array) {
 	this.array = array
+	for (var i = this.array.length - 1 ; i > 0; i--) {
+		for (var j = i; j < this.array.length; j++) {
+			if (this.array[j-1] > this.array[j]) {
+				tempInt = this.array[j]
+				this.array[j] = this.array[j-1]
+				this.array[j-1] = tempInt
+			}
+		}
+	}
 }
 
 State.prototype = {
-	getArray: function() {
-		
-	}
+	
 }
