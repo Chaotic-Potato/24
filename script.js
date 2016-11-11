109ac3605e97112a3833c4b6b061cb11156577c6
Chaotic-Potato <andy825825@gmail.com>
2016-11-10 21:51:44 -0600

    Fixed sorting algorithm

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ var State = function (array) {
 
 State.prototype = {
 	getArray: function() {
-		temp = this.array
-		for (var i = 1; i < this.array.length - 1; i++) {
-			for (var j = i; j < this.array.length; j++) {
-				if (temp[j - 1] > temp[j]) {
-					tempInt = temp[j]
-					temp[j] = temp[j -1]
-				   	temp[j - 1] = tempInt	
-				}
-			}
-		}
-		return temp
+		
 	}
 }
